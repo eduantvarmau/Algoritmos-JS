4f669287531c5e843a4dca4079c5577e438d52c9
eduantvarmau <eduant.varmau@gmail.com>
2022-02-15 18:47:21 -0600

    Actualice el ejercicio dos

--- Ejercicios/dos.js
+++ Ejercicios/dos.js
@@ -9,11 +9,23 @@ Guardar tu clave de elector
 Guardar el nombre de tu mascota*/
 
 
-let edad
-const miNombre
-let direccion
-let precioBolillo
-const fechaNacimiento
-const claveElector
-const nombreMascota
+const nombre= prompt("Ingresa tu nombre");
+let edad = prompt("Ingresa tu edad");
+
+let direccion = prompt("Ingresa tu direccion");
+let precioBolillo = prompt("Ingresa el precio de un bolillo");
+const fechaNacimiento = prompt("Ingresa tu fecha de nacimiento: mes/dia/año");
+const claveElector = prompt("Ingresa tu clave de elector");
+const nombreMascota = prompt("Ingresa el nombre de tu mascota");
+
+console.log ("Tu nombre es: " + nombre + ". Tu edad: " + edad + ". Vives en: " + direccion + 
+". El precio del bolillo es: " + precioBolillo + ". Tu fecha de nacimiento es:" + fechaNacimiento + 
+". Con calve de elector: " + claveElector + " Y el nombre de tu mascota es: " + nombreMascota);
+
+alert("Tu nombre es: " + nombre  + "Tu edad: " + edad + ". Vives en: " + direccion + 
+". El precio del bolillo es: " + precioBolillo + ". Tu fecha de nacimiento es:" + fechaNacimiento + 
+". Con calve de elector: " + claveElector + " Y el nombre de tu mascota es: " + nombreMascota);
+
+
+
 
